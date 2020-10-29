313e78ed0ba969c3c55c2cc2ecd1d88aaa9c37e1
TJ <ivtjt78@gmail.com>
2020-10-29 16:10:48 -0400

    added student service interface and implementation

---
+++ src/main/java/edu/rit/auth/service/Impl/StudentServiceImplementation.java
@@ -0,0 +1,27 @@
+package edu.rit.auth.service.Impl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+import edu.rit.auth.dao.UserRepository;
+import edu.rit.auth.service.StudentService;
+
+@Service
+public class StudentServiceImplementation implements StudentService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(StudentServiceImplementation.class);
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public String getStudentPage(Model model) {
+        LOG.debug("Returning students template");
+        model.addAttribute("students", userRepository.findAll());
+        return "students";
+    }
+    
+}
